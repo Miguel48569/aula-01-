110eb4bce6490772ce43d8eedb0889d29f965314
Miguel Leite <162582246+Miguel48569@users.noreply.github.com>
2024-03-07 11:05:23 -0300

    Add files via upload

---
+++ Aula 1.py
@@ -0,0 +1,23 @@
+# %%
+nome= "Miguel"
+
+# %%
+idade= 17
+
+# %%
+print('Olá,', nome+'!')
+
+# %%
+print ('meu nome é', nome, nome2,  'e tenho', idade, 'anos')
+
+# %%
+nome2= 'Leite'
+dev= True
+
+# %% [markdown]
+# Aula 1
+
+# %%
+print(nome, nome2)
+
+
